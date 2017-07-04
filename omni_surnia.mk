83db9ad7cff1fef2c88a063b45a9b1ccf2eb6ff3
DroidFreak32 <rushabshah32@gmail.com>
2017-07-04 23:08:37 +0530

    Include gsm.mk and DevTools

--- omni_surnia.mk
+++ omni_surnia.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 $(call inherit-product, device/motorola/surnia/full_surnia.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
@@ -36,5 +37,7 @@ PRODUCT_PACKAGES += \
 # It's not the size but, welp
 TARGET_BOOTANIMATION_SIZE := 480x320
 
+# This adds Dev Tools in Developer settings
+TARGET_LOW_RAM_DEVICE := true
 WITH_SU := true
 
